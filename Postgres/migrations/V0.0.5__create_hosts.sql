2bce9d3569910bf826204258553a9add0c7c486b
Saradhi Vemuri <saradhi@zurohq.com>
2021-09-05 08:49:21 -0400

    Made hostname primary key.

--- Postgres/migrations/V0.0.5__create_hosts.sql
+++ Postgres/migrations/V0.0.5__create_hosts.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS central.hosts;
 CREATE TABLE central.hosts
 (
-	id SERIAL NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL,
 	inserted_ts TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_ts TIMESTAMP NOT NULL,
 
 	environment VARCHAR(3) NOT NULL, --Values will be DEV,QAT,UAT,PRD
 	type VARCHAR(25) NOT NULL,
 	purpose VARCHAR(25) NOT NULL, 
-	hostname VARCHAR(255)  NOT NULL,
+	hostname VARCHAR(255)  NOT NULL PRIMARY KEY,
 	description VARCHAR(1000)  NOT NULL
 );
 CREATE TRIGGER updated_ts_trigger BEFORE INSERT OR UPDATE ON central.hosts FOR EACH ROW EXECUTE PROCEDURE  public.update_updated_ts();
