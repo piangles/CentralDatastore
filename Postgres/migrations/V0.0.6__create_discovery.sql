1a2ec2c9180809a5fe6ac6ee6227883617b48c2d
Saradhi Vemuri <saradhi@zurohq.com>
2021-09-05 08:52:05 -0400

    Enforced composite primary keys.

--- Postgres/migrations/V0.0.6__create_discovery.sql
+++ Postgres/migrations/V0.0.6__create_discovery.sql
@@ -1,13 +1,15 @@
 DROP TABLE IF EXISTS central.discovery;
 CREATE TABLE central.discovery
 (
-	id SERIAL NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL,
 	inserted_ts TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_ts TIMESTAMP NOT NULL,
 	
 	environment VARCHAR(3) NOT NULL,
 	service_name VARCHAR(50) NOT NULL,
 	name VARCHAR(250) NOT NULL,
-	value VARCHAR(1000) NOT NULL
+	value VARCHAR(1000) NOT NULL,
+	
+	 PRIMARY KEY(environment, service_name, name)
 );
 CREATE TRIGGER updated_ts_trigger BEFORE INSERT OR UPDATE ON central.discovery FOR EACH ROW EXECUTE PROCEDURE  public.update_updated_ts();
--- Postgres/migrations/V0.0.6__create_tier1_configuration.sql
+++ Postgres/migrations/V0.0.6__create_tier1_configuration.sql
@@ -1,13 +1,15 @@
 DROP TABLE IF EXISTS central.tier1_configuration;
 CREATE TABLE central.tier1_configuration
 (
-	id SERIAL NOT NULL PRIMARY KEY,
+	id SERIAL NOT NULL,
 	inserted_ts TIMESTAMP NOT NULL DEFAULT NOW(),
 	updated_ts TIMESTAMP NOT NULL,
 	
 	environment VARCHAR(3) NOT NULL,
 	service_name VARCHAR(50) NOT NULL,
 	name VARCHAR(250) NOT NULL,
-	value VARCHAR(1000) NOT NULL
+	value VARCHAR(1000) NOT NULL,
+	
+	PRIMARY KEY(environment, service_name, name)
 );
 CREATE TRIGGER updated_ts_trigger BEFORE INSERT OR UPDATE ON central.tier1_configuration FOR EACH ROW EXECUTE PROCEDURE  public.update_updated_ts();
